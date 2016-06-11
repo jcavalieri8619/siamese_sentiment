96ef966020596af1e207343398c9aa5c551c03e2
John P Cavalieri <jcavalieri8619@gmail.com>
2016-06-11 02:16:53 -0400

    data SGD

--- data_perturbing.py
+++ data_perturbing.py
@@ -35,58 +35,61 @@ def identify_highprob_subset( model, X_dev, y_dev, subset_size ):
 
 import theano
 import theano.tensor as T
+import copy
 
 
-def data_SGD( trained_model, best_inputs, loss_func, batch_size = 20, **kwargs ):
+def data_SGD( trained_model, highProb_subset, loss_func, batch_size = 20,
+              **kwargs ):
+	"""
 
+	:param trained_model:
+	:param highProb_subset:
+	:param loss_func:
+	:param batch_size:
+	:param num_epochs:
+	:param kwargs:
+	:return:
+	"""
 
-
-	# D = perturb_data[ (batch_itr) * batch_size: (batch_itr + 1) * batch_size ]
-	#
-	# Xembed_batch = np.stack( [ x[ 0 ] for x in D ] )
-	# y_batch = np.asarray( [ x[ 1 ] for x in D ] )
-	#
-	# Xembed_batch = K.T._shared( Xembed_batch, name = 'Xembed_batch' )
-	#
-	# predictions = trained_model.predict_proba( Xbatch.get_value( ), batch_size )
-
-
-	# X_train, y_train = training_data[ (batch_itr) * batch_size: (batch_itr + 1) * batch_size ]
-
-
-	# shared_data=[]
-	# for elem in perturb_data:
-	# 	shared_data=theano.shared(elem,name = "X_perturb")
-	#
-	# for X_perturb in shared_data:
-
-	# X = T.dmatrix( "X_dataSGD" )
-	# y = T.dvector( "y_dataSGD" )
-
-	X_list = best_inputs
+	X_list = highProb_subset
 	dim1, dim2 = X_list[ 0 ].shape
-	X_embed = np.zeros( (len( X_list ), dim1, dim2) )
-	y_embed = np.ones( (dim1, 1) )
+	X = np.zeros( (len( X_list ), dim1, dim2) )
+	targets = np.ones( (dim1, 1) )
 
 	for indx, matrix in enumerate( X_list ):
-		X_embed[ indx ] = matrix
+		X[ indx ] = matrix
+
+	X_orig = copy.deepcopy( X )
 
 	beta = 0.01
 	alpha = 0.01
 
-	X_embed = theano.shared( X_embed, name = 'X_embed' )
+	X = theano.shared( X, name = 'X_designMatrix' )
+	y = T.ivector( name = 'y_targetvect' )
 
-	p_1 = trained_model.output  # Probability that input is positive review
-	prediction = p_1 > 0.5  # The prediction thresholded
-	xent_loss = -y_embed * T.log( p_1 ) - (1 - y_embed) * T.log( 1 - p_1 )  # Cross-entropy loss function
-	mean_xent_loss = -1 * xent_loss.mean( ) + beta * T.sqr( trained_model.input.norm( 2 ) )
+	posClass_probability = trained_model.output  # Probability that input is positive class
+	prediction = posClass_probability > 0.5  # The prediction thresholded at 0.50
+	neg_crossEntropy_loss = -1 * (-y * T.log( posClass_probability ) - (1 - y) * T.log( 1 - posClass_probability )
+	                              )
+	mean_neg_crossEntropy_loss = neg_crossEntropy_loss.mean( ) + beta * (1)
 
-	dL_dX = T.grad( mean_xent_loss, [ trained_model.input ] )
+	# gradient of mean negative cross entropy loss wrt inputs--not weights
+	dLoss_dX = T.grad( mean_neg_crossEntropy_loss, [ trained_model.input ] )
 
 	train = theano.function(
-			inputs = [ X_embed, y_embed ],
-			outputs = [ prediction, mean_xent_loss ],
-			updates = ((X_embed, X_embed - alpha * dL_dX),) )
+			inputs = [ trained_model.input, y ],
+			outputs = [ prediction, mean_neg_crossEntropy_loss ],
+			updates = ((X, X - alpha * dLoss_dX),),
+			name = "train_data_func" )
 
-	for batch_itr in range( (len( training_data ) / batch_size) + 1 ):
-		pass
+	continuePerturbing = True
+	while continuePerturbing:
+		for batch_itr in range( (len( highProb_subset ) / (batch_size)) + 1 ):
+
+			pred, loss = train( [ X[ batch_itr * batch_size:(batch_itr + 1) * batch_size, :, : ],
+			                      targets[ batch_itr * batch_size:(batch_itr + 1) * batch_size, :, : ] ] )
+
+			epsilon = 0.001
+			if T.sqrt( T.sum( T.sqr( X_orig - X ) ) ) >= epsilon:
+				continuePerturbing = False
+				break
