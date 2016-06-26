2fa376915314b0f8f6f291e3c1f0891a46232d51
John P Cavalieri <jcavalieri8619@gmail.com>
2016-06-26 07:51:29 -0400

    updates

--- data_perturbing.py
+++ data_perturbing.py
@@ -10,17 +10,17 @@ import theano.tensor as T
 from network_utils import get_network_layer_output
 
 
-def get_embeddings(model, X):
+def get_embedding_matrices(model, X):
 	return get_network_layer_output(model, X, 0)
 
 
 def identify_highprob_subset(model, X, y, subset_size):
 	"""
 
-	:param model:
-	:param X:
-	:param y:
-	:param subset_size:
+	:param model: trained CNN model with inputType 1hotVectors
+	:param X: design matrix
+	:param y: targets vector
+	:param subset_size: size of high probability subset to perturb
 	:return:
 	"""
 	probs = model.predict_proba(X)
@@ -29,31 +29,27 @@ def identify_highprob_subset(model, X, y, subset_size):
 
 	prob_vs_index = np.concatenate((probs.reshape((-1, 1)), indices), axis=1)
 
-	posTargets = y[y == 1].reshape((-1, 1))
+	posTargets = y.reshape((-1,))
 
-	# posExamples = prob_vs_index[ np.isclose(prob_vs_index[ :, 1 ], 1), : ]
-	posExamples = prob_vs_index[posTargets, :].reshape((-1, 1)).sort(axis=0)
-	# posExamples.sort(axis=0)
+	posExamples = prob_vs_index[posTargets == 1, :]
+	posExamples.sort(axis=0)
 
-	HPindices = posExamples[:, 1]
+	highProb_indices = posExamples[:, 1]
 
-	X_subset = X[np.array(HPindices[-1:-subset_size:-1, 0], dtype='int32'), :, :]
+	X_subset = X[highProb_indices[-1:-subset_size:-1], :, :]
 
-	remaining_X = X[np.array(HPindices[-subset_size:0:-1, 0], dtype='int32'), :, :]
-	remaining_y = y[np.array(HPindices[-subset_size:0:-1, 0], dtype='int32'), :]
+	remaining_X = X[highProb_indices[-subset_size:0:-1], :, :]
+	remaining_y = y[highProb_indices[-subset_size:0:-1], :]
 
 	return (X_subset, (remaining_X, remaining_y))
 
 
-
-
-
-def data_SGD( trained_model, highProb_subset, loss_func, batch_size = 20,
-              **kwargs ):
+def data_SGD(trained_model, highProb_subset, loss_func, batch_size=20,
+             **kwargs):
 	"""
 
-	:param trained_model:
-	:param highProb_subset:
+	:param trained_model: trained CNN model with inputType embeddingMatrix
+	:param highProb_subset: design matrix consisting of examples predicted correctly with high probability
 	:param loss_func:
 	:param batch_size:
 	:param num_epochs:
@@ -62,23 +58,22 @@ def data_SGD( trained_model, highProb_subset, loss_func, batch_size = 20,
 	"""
 
 	X_list = highProb_subset
-	dim1, dim2 = X_list[ 0 ].shape
-	X = np.zeros( (len( X_list ), dim1, dim2) )
-	targets = np.ones( (dim1, 1) )
+	dim1, dim2 = X_list[0].shape
+	X = np.zeros((len(X_list), dim1, dim2))
+	targets = np.ones((dim1, 1))
 
-	for indx, matrix in enumerate( X_list ):
-		X[ indx ] = matrix
-
+	for indx, matrix in enumerate(X_list):
+		X[indx] = matrix
 
 	beta = 0.01
 	alpha = 0.01
 
-	X = theano.shared( X, name = 'X_designMatrix' )
-	y = T.ivector( name = 'y_targetvect' )
+	X = theano.shared(X, name='X_designMatrix')
+	y = T.ivector(name='y_targetvect')
 
 	posClass_probability = trained_model.output  # Probability that input is positive class
 	prediction = posClass_probability > 0.5  # The prediction thresholded at 0.50
-	neg_crossEntropy_loss = -1 * (-y * T.log( posClass_probability ) - (1 - y) * T.log( 1 - posClass_probability )
+	neg_crossEntropy_loss = -1 * (-y * T.log(posClass_probability) - (1 - y) * T.log(1 - posClass_probability)
 	                              )
 	cost = neg_crossEntropy_loss.mean()
 
@@ -86,10 +81,10 @@ def data_SGD( trained_model, highProb_subset, loss_func, batch_size = 20,
 	dCost_dX = T.grad(cost, [trained_model.input])
 
 	train = theano.function(
-			inputs = [ trained_model.input, y ],
+		inputs=[trained_model.input, y],
 		outputs=[prediction, cost],
 		updates=((X, X - alpha * dCost_dX),),
-			name = "train_data_func" )
+		name="train_data_func")
 
 	for batch_itr in range((len(highProb_subset) / (batch_size)) + 1):
 
@@ -101,8 +96,4 @@ def data_SGD( trained_model, highProb_subset, loss_func, batch_size = 20,
 
 
 def dataSGD_test():
-	from CNN_network_architecture import build_CNN_model
-	fullModel, _, _, Xd, yd = build_CNN_model(weight_path='./CNN_weights.h5')
-	Model = build_CNN_model(True, False, weight_path='./noembed.h5')
-
-	embeddings = get_embeddings(fullModel, Xd)
+	pass
